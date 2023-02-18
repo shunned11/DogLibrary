85d5b15e809be0fdc66871657a33c0a566ac3805
Anush Shand <anush.shand@byjus.com>
2023-02-19 01:55:56 +0530

    Basic Functionality Added

---
+++ app/src/main/java/com/example/dogimages/ui/main/MainFragment.kt
@@ -0,0 +1,38 @@
+package com.example.dogimages.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.dogimages.R
+import com.example.dogimages.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding= DataBindingUtil.inflate(inflater,R.layout.fragment_main,container,false)
+        binding.mainViewModel=viewModel
+        binding.lifecycleOwner=this
+        return binding.root
+    }
+
+}
