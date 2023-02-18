775209b89bb214fdf9358cc0032bcc46fb9951a6
Anush Shand <anush.shand@byjus.com>
2023-02-19 02:45:25 +0530

    Updated Currentindex so that it can be accessed

--- DogImageLibrary/src/main/java/com/example/dogimagelibrary/DogImages.kt
+++ DogImageLibrary/src/main/java/com/example/dogimagelibrary/DogImages.kt
@@ -4,37 +4,37 @@ package com.example.dogimagelibrary
 import com.example.dogimagelibrary.network.DogApi
 object DogImages {
     private var imageList= mutableListOf<String>()
-    private var _currentIndex:Int = 0
-    val currentIndex: Int get() = _currentIndex
+    var currentIndex:Int=0
+        private set
 
 
     suspend fun getImage():List<String>{
         imageList += (DogApi.retrofitService.getDogs(1).imgSrcUrl)
-        _currentIndex= imageList.lastIndex
+        currentIndex= imageList.lastIndex
         return listOf(imageList[currentIndex])
         val x=Math.abs(0)
      }
 
     suspend fun getImages(number: Int):List<String>{
         imageList+=DogApi.retrofitService.getDogs(number).imgSrcUrl
-        _currentIndex= imageList.lastIndex
+        currentIndex= imageList.lastIndex
         return imageList.subList(imageList.lastIndex-number+1, currentIndex)
     }
 
     suspend fun getNextImage():List<String>{
         if(currentIndex==imageList.lastIndex || imageList.lastIndex==-1) {
             imageList += (DogApi.retrofitService.getDogs(1).imgSrcUrl)
-            _currentIndex= imageList.lastIndex
+            currentIndex= imageList.lastIndex
         }
         else{
-            _currentIndex++
+            currentIndex++
         }
         return listOf(imageList[currentIndex])
     }
 
     fun getPreviousImage(): List<String> {
         if(currentIndex!=0)
-            _currentIndex--
+            currentIndex--
         return listOf(imageList[currentIndex])
     }
 }
